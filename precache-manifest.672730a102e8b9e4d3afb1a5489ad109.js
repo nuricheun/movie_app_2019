659dee9e1bd33cb66779d17d1046d235a60b4297
aliglaser <nooti777712@gmail.com>
2019-09-08 13:09:04 -0700

    Updates

---
+++ precache-manifest.672730a102e8b9e4d3afb1a5489ad109.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a8aa78edddaafc87a0db3ba8dae9f8ee",
+    "url": "/movie_app_2019/index.html"
+  },
+  {
+    "revision": "de6edd63afb94bf6a1f1",
+    "url": "/movie_app_2019/static/css/main.e083b577.chunk.css"
+  },
+  {
+    "revision": "886df7fb1256cda95647",
+    "url": "/movie_app_2019/static/js/2.585b84d4.chunk.js"
+  },
+  {
+    "revision": "de6edd63afb94bf6a1f1",
+    "url": "/movie_app_2019/static/js/main.e6b6b1d9.chunk.js"
+  },
+  {
+    "revision": "30a14d7c554eb6584568",
+    "url": "/movie_app_2019/static/js/runtime~main.353f3f61.js"
+  }
+]);
